3d14ea5a58579fe7a7d4425943b049e7af1d83fc
merodeadorNocturno <merodeador.nocturno@gmail.com>
2026-04-23 09:59:45 -0600

    Dev: style correction

--- src/interfaces/mod.rs
+++ src/interfaces/mod.rs
@@ -1,43 +1,38 @@
 use crate::classes::{
-  camera::Camera,
-  color::Color,
-  vector::Vector,
-  surfaces::Surfaces,
-  sphere::Sphere,
-  plane::Plane,
+    camera::Camera, color::Color, plane::Plane, sphere::Sphere, surfaces::Surfaces, vector::Vector,
 };
 
 pub struct Ray {
-  pub start: Vector,
-  pub dir: Vector,
+    pub start: Vector,
+    pub dir: Vector,
 }
 
 pub struct Surface {
-  pub roughness: f64,
+    pub roughness: f64,
 }
 
 pub struct Intersection<T> {
-  pub ray: Ray,
-  pub dist: f64,
-  pub thing: T,
-  pub is_null: bool,
+    pub ray: Ray,
+    pub dist: f64,
+    pub thing: T,
+    pub is_null: bool,
 }
 
 pub struct Thing {
-  pub surface: Surface,
+    pub surface: Surface,
 }
 
 pub trait Intersect<T> {
-  fn intersect(self, ray: Ray) -> Intersection<T>;
+    fn intersect(self, ray: Ray) -> Intersection<T>;
 }
 
 pub struct Light {
-  pub pos: Vector,
-  pub color: Color,
+    pub pos: Vector,
+    pub color: Color,
 }
 
 pub struct Scene {
-  pub things: Vec<Intersection<(Sphere, Plane, Surfaces)>>,
-  pub camera: Camera,
-  pub lights: Vec<Light>,
+    pub things: Vec<Intersection<(Sphere, Plane, Surfaces)>>,
+    pub camera: Camera,
+    pub lights: Vec<Light>,
 }
